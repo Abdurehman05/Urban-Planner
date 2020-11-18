996c534ced09653ced1325251946895a368f43d9
Abdu <abdukr5@gmail.com.com>
2020-11-18 15:55:55 -0600

    City constructor added to instantiate cityname mayer and year established

--- City.cs
+++ City.cs
@@ -4,6 +4,12 @@ namespace Planner
 {
     public class City
     {
+        public City()
+        {
+            CityName = "Nashville";
+            Mayor = "John Cooper";
+            YearEstablished = 1830;
+        }
         public string CityName { get; set; }
         public string Mayor { get; set; }
         public int YearEstablished { get; set; }
@@ -12,6 +18,15 @@ namespace Planner
         public void AddBuilding(Building building)
         {
             buildings.Add(building);
+        }
+
+        public void CityInfo()
+        {
+            Console.WriteLine($@"
+            {CityName} 
+           -------------- 
+            Mayor : {Mayor}
+            Year Established : {YearEstablished}");
         }
 
 
